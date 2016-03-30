066b91ce07a2c411fbc1dec856bab0869583d73a
Ashley Aiken <aikena@berea.edu>
2016-03-30 11:57:57 -0400

    Modified the plugin to create a text input field. Renamed URL to Time

--- octoprint_safetytimeout/__init__.py
+++ octoprint_safetytimeout/__init__.py
@@ -7,14 +7,18 @@ class SafetyTimeoutPlugin(octoprint.plugin.StartupPlugin,
 			  octoprint.plugin.TemplatePlugin,
 			  octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("Safety Timeout!!!")
+        self._logger.info("Safety Timeout! (more: %s)" % self._settings.get(["Time"]))
 
     def get_settings_defaults(self):
-	return dict(time= " ")
+        return dict(Time="https://en.wikipedia.org/wiki/Hello_world")
 
-	
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]	
 
 __plugin_name__ = "Safety Timeout"
 __plugin_version__ = "1.0.0"
-__plugin_description__ = "A quick \"Hello World\" After a specified amount of time, this plugin shuts off the temperature of the hotend and the bed"
+__plugin_description__ = "A quick \"Safety feature\" After a specified amount of time, this plugin shuts off the temperature of the hotend and the bed"
 __plugin_implementation__ = SafetyTimeoutPlugin()
