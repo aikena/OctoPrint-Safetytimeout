b29db57de5b1600394f7fd8595fa845b17d2a10f
Ashley Aiken <aikena@berea.edu>
2016-04-06 10:44:32 -0400

    "I tried to implement the timer code in the init file but it did not work so I changed it back to the original. I also removed the nav_bar file because we don't need the Safety Timeout in the Navbar. "

--- octoprint_safetytimeout/__init__.py
+++ octoprint_safetytimeout/__init__.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-import octoprint.plugin
+import octoprint.plugin 
 
 class SafetyTimeoutPlugin(octoprint.plugin.StartupPlugin,
 			  octoprint.plugin.TemplatePlugin,
 			  octoprint.plugin.SettingsPlugin):
+
     def on_after_startup(self):
         self._logger.info("Safety Timeout! (more: %s)" % self._settings.get(["Time"]))
 
@@ -17,7 +18,7 @@ class SafetyTimeoutPlugin(octoprint.plugin.StartupPlugin,
             dict(type="navbar", custom_bindings=False),
             dict(type="settings", custom_bindings=False)
         ]	
-
+   
 __plugin_name__ = "Safety Timeout"
 __plugin_version__ = "1.0.0"
 __plugin_description__ = "A quick \"Safety feature\" After a specified amount of time, this plugin shuts off the temperature of the hotend and the bed"
