714f507c5b3bb32d2740b2519cfe214770417e17
Ashley Aiken <aikena@berea.edu>
2016-04-27 11:37:08 -0400

    Trying to toggle the timers

--- octoprint_safetytimeout/__init__.py
+++ octoprint_safetytimeout/__init__.py
@@ -12,29 +12,42 @@ class SafetyTimeoutPlugin(octoprint.plugin.AssetPlugin,
 			  octoprint.plugin.EventHandlerPlugin,
 			  octoprint.plugin.StartupPlugin,
 			  octoprint.plugin.TemplatePlugin,
-			  octoprint.plugin.SettingsPlugin):
-	
-  version = "D"
+			  octoprint.plugin.SettingsPlugin):	
+  def start(self):
+    print ("Start: %s" % self.initialstart)
+    return self.initialstart
+
+  def go(self):
+    print ("GO: %s" % self.timerstart)
+    return self.timerstart
+
+ 
   def condition(self):
-    temperatures = self._printer.get_current_temperatures()
-    if int(self._settings.get(["Time"])) > 0 and self._printer.get_current_temperatures() != {}:
-      if float(temperatures.get("bed").get("target")) != 0 or float(temperatures.get("tool0").get("target")) != 0:
-        self.makeTimer()
-	return
-    print("Conditions not met")	 
+    if self.initialstart == True:
+      temperatures = self._printer.get_current_temperatures()
+      if int(self._settings.get(["Time"])) > 0 and self._printer.get_current_temperatures() != {}:
+        if float(temperatures.get("bed").get("target")) != 0 or float(temperatures.get("tool0").get("target")) != 0:
+          self.makeTimer()
+	  return
+      print("Conditions not met")
+    else:
+      print("Conditions not met") 	 
   
   def makeTimer (self):
     self.initial = float(self._settings.get(["Time"]))
     self._logger.info("The Timer Has Been Initiated!")
     seconds = self.initial * 60
     print("There are %s seconds left" % seconds)
-    self.countdown = RepeatedTimer(seconds, self.shutdown)
-    self.timer.cancel()
+    self.countdown = RepeatedTimer(seconds, self.shutdown, run_first=False, condition = self.go)
+    self.initialstart = False
+    self.timerstart = True
     self.countdown.start()
 
   def on_after_startup(self):
+    self.initialstart = True
+    self.timerstart = True
     self.initial = float(self._settings.get(["Time"]))
-    self._logger.info("\n\nSafety Timeout set to: Version {1}, {0}\n\n".format(self._settings.get(["Time"]), self.version))
+    #self._logger.info("\n\nSafety Timeout set to: Version {1}, {0}\n\n".format(self._settings.get(["Time"]), self.version))
     self.timer = RepeatedTimer(1.0, self.condition, run_first=True)
     self.timer.start()
     print("Octoprint team")
@@ -50,8 +63,8 @@ class SafetyTimeoutPlugin(octoprint.plugin.AssetPlugin,
     print("The old time is %s " % old_time)
     print("The new time is %s " % new_time)
     if old_time != new_time:
-      self.countdown.cancel()
-      self.timer.start()
+      self.timerstart = False
+      self.initialstart = True
       print("Self.countdown has been cancelled")
   
   def countdown(self):
@@ -76,8 +89,8 @@ class SafetyTimeoutPlugin(octoprint.plugin.AssetPlugin,
     #check to see if the printer is printing 
     if self._printer.is_printing():
       print("The printer is printing!")
-      self.countdown.cancel()
-      self.timer.start()
+      self.timerstart = False
+      self.initialstart = True
       print("self.countdown has been cancelled")
     #if the printer is printing we do not want to interupt it 
     else:
@@ -86,8 +99,8 @@ class SafetyTimeoutPlugin(octoprint.plugin.AssetPlugin,
       print("Bed temperature set to 0")
       self._printer.set_temperature("tool0", 0)
       print("Tool temperature set to 0")
-      self.countdown.cancel()
-      self.timer.start()
+      self.timerstart = False
+      self.initialstart = True
       print("self.countdown has been cancelled")
 
   def get_settings_defaults(self):
