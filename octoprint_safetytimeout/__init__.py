c09fc8fd0c6b1dbb19e9f7d933bb10ecfe99a752
Ashley Aiken <aikena@berea.edu>
2016-04-27 11:59:55 -0400

    I have deleted unnecessary things and commented out the print statements

--- octoprint_safetytimeout/__init__.py
+++ octoprint_safetytimeout/__init__.py
@@ -13,84 +13,51 @@ class SafetyTimeoutPlugin(octoprint.plugin.AssetPlugin,
 			  octoprint.plugin.StartupPlugin,
 			  octoprint.plugin.TemplatePlugin,
 			  octoprint.plugin.SettingsPlugin):	
-#  def start(self):
-#    print ("Start: %s" % self.initialstart)
-#    return self.initialstart
-
-#  def go(self):
-#    print ("GO: %s" % self.timerstart)
-#    return self.timerstart
-
  
   def condition(self):
     if self.initialstart == True:
       temperatures = self._printer.get_current_temperatures()
-      if float(self._settings.get(["Time"])) > 0 and self._printer.get_current_temperatures() != {}:
+      if int(self._settings.get(["Time"])) > 0 and self._printer.get_current_temperatures() != {}:
         if float(temperatures.get("bed").get("target")) != 0 or float(temperatures.get("tool0").get("target")) != 0:
           self.makeTimer()
 	  return
-      print("Conditions not met")
+#      print("Conditions not met")
     else:
-      print("Conditions not met") 	 
+#      print("Conditions not met") 	 
   
   def makeTimer (self):
     self.initial = float(self._settings.get(["Time"]))
     self._logger.info("The Timer Has Been Initiated!")
     seconds = self.initial * 60
-    print("There are %s seconds left" % seconds)
+#    print("There are %s seconds left" % seconds)
     self.countdown = RepeatedTimer(seconds, self.shutdown, run_first=False)
     self.initialstart = False
-#    self.timerstart = True
     self.countdown.start()
 
   def on_after_startup(self):
     self.initialstart = True
-#    self.timerstart = True
-    self.initial = float(self._settings.get(["Time"]))
-    #self._logger.info("\n\nSafety Timeout set to: Version {1}, {0}\n\n".format(self._settings.get(["Time"]), self.version))
+    self.initial = int(self._settings.get(["Time"]))
+
     self.timer = RepeatedTimer(1.0, self.condition, run_first=True)
     self.timer.start()
-    print("Octoprint team")
     # get the temperatures of the tools
-    #temperatures = self._printer.get_current_temperatures()
-    #self._logger.info("The Temperature is: %s" % temperatures)
-    #self._logger.info("this is stuff: %s" % temperatures.keys())
-  
-#  def countdown(self):
-    #TODO if t is 0 (the default), we need to end this function because that means the user did not want a timer set
-    # t comes in as a string from the get.
-#    t = self._settings.get(["Time"]) 
-    # Convert to a number; could be a float.
-#    t = float(t)
-    # Further cast down to an int.
-#    t = int(t) 
-#    t = t*60
-#    while t >= 0:
-#      mins, secs = divmod(t, 60)
-#      timeformat = '{:02d}:{:02d}'.format(mins, secs)
-#      sys.stdout.write("\r" + timeformat)
-#      sys.stdout.flush()
-#      time.sleep(1)
-#      t -= 1
-#      print('Goodbye!\n\n\n\n\n')
-  
+ 
+
   def shutdown(self):
     #check to see if the printer is printing 
     if self._printer.is_printing():
       print("The printer is printing!")
-#      self.timerstart = False
       self.initialstart = True
-      print("self.countdown has been cancelled")
+#      print("self.countdown has been cancelled")
     #if the printer is printing we do not want to interupt it 
     else:
       #else, we need to set the temperature of the bed and the tool to 0 
       self._printer.set_temperature("bed", 0)
-      print("Bed temperature set to 0")
+#      print("Bed temperature set to 0")
       self._printer.set_temperature("tool0", 0)
-      print("Tool temperature set to 0")
-#      self.timerstart = False
+#      print("Tool temperature set to 0")
       self.initialstart = True
-      print("self.countdown has been cancelled")
+#      print("self.countdown has been cancelled")
 
   def get_settings_defaults(self):
     #set the default time
@@ -98,7 +65,6 @@ class SafetyTimeoutPlugin(octoprint.plugin.AssetPlugin,
 
   def get_template_configs(self):
     return [
-      # dict(type="navbar", custom_bindings=False),
       dict(type="settings", custom_bindings=False)
     ]
 
