31f5792732fc1f090bff6fa8438fc5d703260504
Ashley Aiken <aikena@berea.edu>
2016-05-04 16:00:55 -0400

    Cleaned up the init file by erasing print statements and adding docstrings

--- octoprint_safetytimeout/__init__.py
+++ octoprint_safetytimeout/__init__.py
@@ -15,54 +15,52 @@ class SafetyTimeoutPlugin(octoprint.plugin.AssetPlugin,
 			  octoprint.plugin.SettingsPlugin):	
  
   def condition(self):
+    ''' This function is to check and see if the temperature and time are set above 0, and a timer does not already exist.
+	If they are: Starts a timer
+	If they are not: It does nothing'''
     if self.initialstart == True:
       temperatures = self._printer.get_current_temperatures()
       if int(self._settings.get(["Time"])) > 0 and temperatures != {}:
         if float(temperatures.get("bed").get("target")) != 0 or float(temperatures.get("tool0").get("target")) != 0:
           self.makeTimer()
-	  return
-#      print("Conditions not met")
-#    else:
-#      print("Conditions not met") 	 
+    return
   
   def makeTimer (self):
+    ''' This function creates a timer instance by grabbing the most recent user specified time'''
     self.countdowndefined = True
     self.initial = int(self._settings.get(["Time"]))
     self._logger.info("The Timer Has Been Initiated!")
     seconds = self.initial * 60
-#    print("There are %s seconds left" % seconds)
     self.countdown = RepeatedTimer(seconds, self.shutdown, run_first=False)
     self.initialstart = False
     self.countdown.start()
 
   def on_after_startup(self):
+    ''' Upon server startup, we define the class variables and create our initial timer that constantly checks to see if a safety timeout is needed '''
     self.countdowndefined = False
     self.initialstart = True
     self.initial = int(self._settings.get(["Time"]))
-
     self.timer = RepeatedTimer(1.0, self.condition, run_first=True)
     self.timer.start()
-    # get the temperatures of the tools
- 
+  
   def on_settings_save(self, data):
+    ''' We overload this function in case someone changes the time after a timer has already been set. It will cancel the old timer, and resume checking to see if another one needs to be created'''
     old_time = self._settings.get(["Time"])
-
     octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
-
     new_time = self._settings.get(["Time"])
     if old_time != new_time:
       if self.countdowndefined == True:
         self.countdown.cancel()
-     #   print("Old timer cancelled")
+        self._logger.info("The Timer Has Been cancelled!")
         seconds = int(new_time) * 60
-     #   print("New timer initiated")
-     #   print(seconds)
-     # self.countdown = RepeatedTimer(seconds, self.shutdown, run_first = False)
-     # self.countdown.start()
         self.initialstart = True
 
   def shutdown(self):
+    ''' Checks to see if machine is printing.
+	If it is: Resumes checking to see if a timer is needed.
+	If it is not: Sets the bed to 0'''
     self.countdown.cancel()
+    self.countdowndefined = False
     #check to see if the printer is printing 
     if self._printer.is_printing():
 #      print("The printer is printing!")
